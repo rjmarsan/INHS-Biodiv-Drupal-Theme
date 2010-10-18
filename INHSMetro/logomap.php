f2a93ff8c1ad1836420f602ae10c4493af0a8381
RJ <rjmarsan@gmail.com>
2010-10-18 18:15:17 -0500

    Moving everything here.

---
+++ INHSMetro/logomap.php
@@ -0,0 +1,12 @@
+
+
+<MAP NAME="map">
+
+ <area shape="RECT" coords="852,134,892,182" href="http://illinois.edu" alt="Illinois">  
+ <area shape="RECT" coords="656,140,848,163" href="http://www.inhs.illinois.edu/" alt="INHS">  
+ <area shape="RECT" coords="584,164,848,183" href="http://www.inrs.illinois.edu/" alt="INRS">  
+ <area shape="RECT" coords="366,42,867,118" href="/" alt="Tinerella Lab">  
+</MAP>
+
+
+
